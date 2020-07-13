8e8fb44dad7be782e1b3ca06026cc162d7244cff
kumiko.anzai <anzai.k@opst.co.jp>
2020-07-13 19:26:59 +0900

    Initial commit

---
+++ src/main/java/code/alieaters/Application.java
@@ -0,0 +1,10 @@
+package code.alieaters;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
