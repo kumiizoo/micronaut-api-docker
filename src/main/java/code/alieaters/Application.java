f6aa63e48623b51d65ae3a03963e94a7f0d9452b
kumiko.anzai <anzai.k@opst.co.jp>
2020-07-21 16:18:11 +0900

    feat: add aliyun functio compute common handler

--- src/main/java/code/alieaters/Application.java
+++ src/main/java/code/alieaters/Application.java
@@ -1,10 +1,20 @@
 package code.alieaters;
-
+import com.aliyun.fc.runtime.Context;
+import com.aliyun.fc.runtime.StreamRequestHandler;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class Application implements StreamRequestHandler {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class);
+    }
+
+    @Override
+    public void handleRequest(
+            InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        outputStream.write(new String("hello world").getBytes());
     }
 }
